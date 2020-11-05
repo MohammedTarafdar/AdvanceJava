7bcfe9cb77d645e912da23ae92008a7335b736c7
MohammedTarafdar <mtarafdar96@gmail.com>
2020-11-04 23:19:53 -0500

    first commit

---
+++ src/main/java/com/basic/HelloJava.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+/*	"public" and "class" are two java keywords. The "public" keyword is an access 
+ * modifier and "class" keyword is used ti define a class
+ * 
+ */
+
+public class HelloJava {
+	
+	public static void main(String [] args) {
+	
+	System.out.println("Hello World !");
+		
+	
+	}
+	
+
+}
